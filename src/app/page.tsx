aff2e5352cd797bf15df41a235720594c1fc7e09
AXer-neil <neil@axchange.co>
2024-11-21 16:56:11 +0900

    [Fix] : typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
               } text-white px-4 py-2 rounded hover:opacity-90 transition-opacity`}
               onClick={() => setDoesDisplayPosts(true)}
             >
-              Read Blog
+              Posts
             </button>
             <button
               className={`${
@@ -44,7 +44,7 @@ export default function Home() {
               } text-white px-4 py-2 rounded hover:opacity-50 transition-opacity`}
               onClick={() => setDoesDisplayPosts(false)}
             >
-              View Projects
+              Projects
             </button>
           </div>
         </div>
